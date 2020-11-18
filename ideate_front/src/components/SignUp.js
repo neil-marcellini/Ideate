ec28c2564b056aba27ad67a600ac4981ff528e23
neil-marcellini <neil.marcellini@gmail.com>
2020-11-17 18:01:52 -0800

    added individual errors for sign up

--- ideate_front/src/components/SignUp.js
+++ ideate_front/src/components/SignUp.js
@@ -28,9 +28,10 @@ export default function SignUp() {
     const classes = useStyles()
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [msg, setMsg] = useState(null);
-
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated) 
+    const [emailMsg, setEmailMsg] = useState(null);
+    const [emailError, setEmailError] = useState(false)
+    const [passwordMsg, setPasswordMsg] = useState(null);
+    const [passwordError, setPasswordError] = useState(false)
     const error = useSelector(state => state.error) 
 
     const dispatch = useDispatch();
@@ -38,9 +39,27 @@ export default function SignUp() {
     useEffect(()=> {
         // Check for register error
         if (error.id === 'SIGNUP_FAIL') {
-            setMsg(error.msg.msg);
+            if (error.msg.field === 'both') {
+                setEmailMsg(error.msg.msg)
+                setPasswordMsg(error.msg.msg)
+                setEmailError(true)
+                setPasswordError(true)
+            } else if (error.msg.field === 'email'){
+                setEmailError(true)
+                setEmailMsg(error.msg.msg)
+                setPasswordMsg(null)
+                setPasswordError(false)
+            } else if (error.msg.field === 'password') {
+                setPasswordMsg(error.msg.msg)
+                setPasswordError(true)
+                setEmailMsg(null)
+                setEmailError(false)
+            }
         } else {
-            setMsg(null);
+            setEmailMsg(null)
+            setEmailError(false)
+            setPasswordMsg(null)
+            setPasswordError(false)
         }
     },[error])
 
@@ -51,12 +70,12 @@ export default function SignUp() {
                 <Card className={classes.card}>
                     <CardHeader title="Sign Up"/>
                     <CardContent>
-                        <TextField error={msg != null} label="Email" variant="filled" onChange={(e) => {
-                            setEmail(e.target.value)}} helperText={msg} />
+                        <TextField error={emailError} label="Email" variant="filled" onChange={(e) => {
+                            setEmail(e.target.value)}} helperText={emailMsg} />
                         <br />
                         <br />
-                        <TextField error={msg != null} label="Password" type="password" variant="filled" onChange={(e) => {
-                            setPassword(e.target.value)}} helperText={msg}/>
+                        <TextField error={passwordError} label="Password" type="password" variant="filled" onChange={(e) => {
+                            setPassword(e.target.value)}} helperText={passwordMsg}/>
                     </CardContent>
                     <CardActions>
                         <Button onClick={() => dispatch(signUp({email, password}))} variant="contained" color="primary">Submit</Button>
