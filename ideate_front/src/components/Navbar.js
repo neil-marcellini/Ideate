4ec210a007929eb306b82f4e171a1d9355f8e9d1
neil-marcellini <neil.marcellini@gmail.com>
2020-11-19 17:41:43 -0800

    navbar style improvements

--- ideate_front/src/components/Navbar.js
+++ ideate_front/src/components/Navbar.js
@@ -1,27 +1,65 @@
-import React, { useEffect} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom';
-import './Navbar.css';
 import {logOut} from '../actions/authActions'
 import { useDispatch, useSelector } from "react-redux";
-import store from '../store'
-import { loadUser} from '../actions/authActions'
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles({
+    navbar: {
+        backgroundColor: "#287eff",
+        color: "white",
+        width: "100%",
+        height: "15%",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    
+    navLinks: {
+        padding: "0 1rem",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "40%",
+        '& li': {
+            padding: "0.5rem",
+            color: "white",
+            textDecoration: "none",
+            listStyle: "none"
+        }
+    },
+    
+    navName: {
+        padding: "0.5rem",
+        fontSize: "300%"
+    },
+    a: {
+        
+    }
+});
 
 export default function Navbar() {
+    const classes = useStyles()
     const dispatch = useDispatch()
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated) 
     
     return (
-        <header className="Nav-header">
-            <h1 className="Nav-name">Ideate</h1>
+        <nav className={classes.navbar}>
+            <h1 className={classes.navName}>Ideate</h1>
             {isAuthenticated &&
-                <div>
-                    <Link className="nav-items" to="/idea">Idea</Link>
-                    <Link className="nav-items" to="/topics">Topics</Link>
-                    <h3 className="nav-items" onClick={() => dispatch(logOut)}>Log Out</h3>
-                </div>    
+                <ul className={classes.navLinks}>
+                    <li><Link to="/idea" style={{fontSize: "2em", textDecoration: "none", color: "white"}}>Idea</Link></li>
+                    <li><Link to="/topics" style={{fontSize: "2em", textDecoration: "none", color: "white"}}>Topics</Link></li>
+                    <li><h4 onClick={() => dispatch(logOut)}>Log Out</h4></li>
+                </ul>    
             }
-            
-        </header>
+        </nav>
+
     )
 
 }
