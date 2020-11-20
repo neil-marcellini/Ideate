edd752c66b7f2ca04ed931b799d57b5659eca1de
neil-marcellini <neil.marcellini@gmail.com>
2020-11-20 13:57:32 -0800

    image upload UI looking decent

--- ideate_front/src/components/CreateProfile.js
+++ ideate_front/src/components/CreateProfile.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Paper, TextareaAutosize, TextField, Typography, Avatar } from '@material-ui/core'
+import { Paper, TextareaAutosize, TextField, Typography, Avatar, Button } from '@material-ui/core'
+import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
+import ImageUploader from 'react-images-upload';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -9,9 +11,22 @@ const useStyles = makeStyles({
         justifyContent: "center",
         alignItems: "center"
     },
+    paper: {
+        width: "40%",
+        padding: "1em",
+    },
     namePhoto: {
-        display: "flex",
-        flexDirection: "row"
+        display: "grid",
+        gridTemplateColumns: "auto auto",
+        gridColumnGap: "1em",
+        alignItems: "center"
+    },
+    imageUpload: {
+        width: "10em",
+    },
+    photoDisplay: {
+        width: "5em",
+        height: "5em"
     }
 })
 
@@ -20,16 +35,26 @@ export default function CreateProfile() {
     const classes = useStyles()
     return (
         <form className={classes.formContainer}>
-            <Paper>
+            <Paper className={classes.paper}>
                 <Typography variant="h3" style={{textDecoration: "underline"}}>Create Your Profile</Typography>
                 <br />
-                <div className={classes.namePhoto}>
-                    <Typography variant="h5">Profile Name</Typography>
-                    <Avatar>H</Avatar>
-                </div>
-                <br />
+                <Typography variant="h5">Profile Name</Typography>
                 <TextField variant="filled"/>
-                <br />
+                <Typography variant="h5">Photo</Typography>
+                <div className={classes.namePhoto}>
+                    <ImageUploader className={classes.imageUpload}
+                            withIcon={false}
+                            withPreview={true}
+                            label=""
+                            buttonText="Upload"
+                            onChange={()=>{}}
+                            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                            maxFileSize={5242880}
+                            singleImage
+                    />
+                    <Avatar className={classes.photoDisplay} variant="rounded">H</Avatar>
+                    
+                </div>
                 <br />
                 <Typography variant="h5">Bio</Typography>
                 <Typography>Let us know who you are!</Typography>
