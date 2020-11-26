214cdeacf4d9a4552d66c1978c5bfd742298f0c4
neil-marcellini <neil.marcellini@gmail.com>
2020-11-25 17:18:10 -0800

    added grid for potential

--- ideate_front/src/components/Potential.js
+++ ideate_front/src/components/Potential.js
@@ -1,12 +1,15 @@
-import React, {useState, useRef} from 'react'
+import React, {useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Slider from '@material-ui/core/Slider';
 
 
 const useStyles = makeStyles({
-    container : {
+    containerGrid: {
         padding: "1rem",
-        width: "fit-content"
+        width: "fit-content",
+        display: "grid",
+        gridTemplateColumns: "auto auto",
+        gridTemplateRows: "auto auto"
     },
     box: {
         boxSizing: "border-box",
@@ -15,7 +18,17 @@ const useStyles = makeStyles({
         position: "relative",
         width: "100px",
         height: "100px",
-        border: "1px solid black"
+        border: "1px solid black",
+        borderCollapse: "collapse",
+
+        '& tr': {
+            border: "1px solid black",
+            borderCollapse: "collapse"
+        },
+        '& td': {
+            border: "1px solid black",
+            borderCollapse: "collapse"
+        }
     },
     symbol: {
         boxSizing: "border-box",
@@ -25,7 +38,11 @@ const useStyles = makeStyles({
         height: "16px",
         position: "absolute",
         left: props => props.x,
-        top: props => props.y
+        bottom: props => props.y
+    },
+    xSlider: {
+        width: "100px",
+        gridColumn: "2 / 2"
     }
 })
 
@@ -35,19 +52,25 @@ export default function Potential() {
     const offset = 8
     
     const [x, setX] = useState(50)
-    const [y, setY] = useState(0)
-    const props = {x: `${x-offset}%`, y: `${y-offset}%`}
+    const [y, setY] = useState(50)
+    const props = {x: `${x-offset}%`, y: `${y}%`}
     const classes = useStyles(props)
 
     
     
 
     return (
-        <div className={classes.container}>
-            <div className={classes.box}>
-                <div className={classes.symbol}>💡</div>
-            </div>
-            <Slider value={x} onChange={(e, newValue) => setX(newValue)} aria-labelledby="continuous-slider" />
+        <div className={classes.containerGrid}>
+            <Slider orientation="vertical" value={y} onChange={(e, newValue) => setY(newValue)} aria-labelledby="continuous-slider" />
+            <table className={classes.box}>
+                <tr>
+                    <td /><td />
+                </tr>
+                <tr>
+                    <td> <div className={classes.symbol}>💡</div></td> <td />
+                </tr>
+            </table>
+            <Slider className={classes.xSlider} value={x} onChange={(e, newValue) => setX(newValue)} aria-labelledby="continuous-slider" />
         </div>
     )
 }
