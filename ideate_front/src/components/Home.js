9c0dfc288d4b08ae59e0abaf027a4c476b1f02a5
neil-marcellini <neil.marcellini@gmail.com>
2020-11-30 12:16:09 -0800

    Added loading spinner on home page

--- ideate_front/src/components/Home.js
+++ ideate_front/src/components/Home.js
@@ -2,17 +2,42 @@ import React, { useState, useEffect } from 'react'
 import Idea from './Idea'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllIdeas } from '../actions/ideaActions'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    loading: {
+        width: "100%",
+        height: "100vh",
+        display: "grid",
+        placeItems: "center"
+    }
+})
 
 export default function Home() {
     const dispatch = useDispatch()
+    const [loading, setLoading] = useState(true)
     const ideas = useSelector(state => state.idea.ideas)
+    const classes = useStyles()
 
     useEffect(() => {
         dispatch(getAllIdeas())
     }, [])
 
+    useEffect(() => {
+        if(ideas.length > 0) {
+            console.log("done loading")
+            setLoading(false)
+        }
+    }, [ideas])
+
     return (
         <>
+            {loading &&
+            <div className={classes.loading}>
+                <CircularProgress />
+            </div>
+            }
             {ideas.map((idea) => (
                 <Idea key={idea.idea_id} idea={idea} />
             ))}
