8ce1aaa52b8ada0034d273ab12de1d4f3c2bc180
neil-marcellini <neil.marcellini@gmail.com>
2020-12-02 13:33:21 -0800

    added sp to get latest iteration data of a topic

--- Ideate_SQL/sp_latest_topic_iterations.sql
+++ Ideate_SQL/sp_latest_topic_iterations.sql
@@ -1,11 +1,17 @@
-/* Gets all iteration descriptions and potential data from all iterations
-that are the latest for their idea. */
-DROP VIEW IF EXISTS latest_iterations_view;
-CREATE VIEW latest_iterations_view AS
+/* Get the latest iteration data for a topic. */
+
+DROP PROCEDURE IF EXISTS sp_latest_topic_iterations;
+delimiter //
+CREATE PROCEDURE sp_latest_topic_iterations(topic_id INT)
+BEGIN
 SELECT Iteration.iteration_id, Iteration.iteration_description, 
 ROUND(AVG(Potential.potential_difficulty)) AS potential_difficulty,
 ROUND(AVG(Potential.potential_brightness)) AS potential_brightness
 FROM Iteration
+INNER JOIN Idea ON Iteration.idea_id = Idea.idea_id
 INNER JOIN Potential ON Iteration.iteration_id = Potential.iteration_id
 WHERE Iteration.iteration_num = (SELECT MAX(iteration_num) FROM Iteration)
-GROUP BY Iteration.idea_id
+AND Idea.topic_id = topic_id
+GROUP BY Iteration.idea_id;
+END //
+delimiter ;
