9e597bef578284c7f39e1c6a7d51cbcdf9033454
neil-marcellini <neil.marcellini@gmail.com>
2020-12-15 12:42:45 -0800

    attempting to use heroku cleardb

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const path = require('path')
 require('dotenv').config();
 
 function createConnection() {
-    const connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    })
+    const connection = mysql.createConnection(process.env.CLEAR_DB_DATABASEURL)
     return connection
 }
 const db = createConnection()
