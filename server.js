2f55e926a4efa06531bccde964f01375093b22fc
neil-marcellini <neil.marcellini@gmail.com>
2021-01-11 10:44:28 -0800

    trying to fix routing issue

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const jwt_secret_key = process.env.JWT_SECRET_KEY
 
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 5000;
-
-const buildPath = path.join(__dirname, './ideate_front/build');
-app.use(express.static(buildPath));
+app.use(morgan('combined'))
 
 const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://pacific-mesa-86012.herokuapp.com']
 const corsOptions = {
@@ -39,7 +36,14 @@ app.use('/api/topic', require('./routes/api/topic'))
 app.use('/api/iteration', require('./routes/api/iteration'))
 
 
-app.use(morgan('combined'))
 
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('ideate_front/build'));
+}
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'ideate_front/build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
